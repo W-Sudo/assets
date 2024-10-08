0d440399e3be95c4ce49582903393c7e6d4e281e
W-Sudo <chwa23061@g.nihon-u.ac.jp>
2024-10-08 18:58:45 +0900

    デバッグ

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,11 @@
 import Nav from "./Nav";
-//import Dove from "./Dove"
+import Dove from "./Dove"
 
 export default function Header(){
     return (
         <header className="header">
             <h1>Birdwatching</h1>
-            <div className="img">
-                <img 
-                src="/src/Images/dove.png"
-                alt="a simple dove logo"
-                />
-            </div>
+            <Dove />
             <Nav />
         </header>
     )
