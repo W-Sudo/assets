6bb6f2975e90e86341da7168dce128bd64c17d0a
W-Sudo <chwa23061@g.nihon-u.ac.jp>
2024-10-08 17:55:16 +0900

    メインコンテンツの追加

---
+++ src/components/Aside.jsx
@@ -0,0 +1,10 @@
+import FavPic from "./FavPic";
+
+export default function Aside(){
+    return (
+        <aside className="aside">
+            <h2>Favourite photos</h2>
+            <FavPic/>
+        </aside>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Article from "./Article";
+import Aside from "./Aside";
+
+export default function Main(){
+    return(
+        <main  className="main">
+            <Article/>
+            <Aside/>
+        </main>
+    )
+}
